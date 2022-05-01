7f41245bf0a68844009f045540fa64e4271b74d0
Endermax200113 <47977493+Endermax200113@users.noreply.github.com>
2022-05-02 02:26:16 +0300

    Разработана шапка главного активити

--- app/src/main/java/com/justmax/virtualstyler/ui/MainActivity.java
+++ app/src/main/java/com/justmax/virtualstyler/ui/MainActivity.java
@@ -1,2 +1,19 @@
-package com.justmax.virtualstyler.ui;public class MainActivity {
+package com.justmax.virtualstyler.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.justmax.virtualstyler.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.mainToolbar);
+    }
 }
